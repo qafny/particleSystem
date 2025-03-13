2bc2bd5b31521380ed54c74eb84f9c5b8d0a98aa
anff <anff@iastate.edu>
2025-03-12 22:44:58 -0500

    Check in QWire, clean qblue. By Fen

---
+++ coq/QBlueSyntax.v
@@ -0,0 +1,12 @@
+Require Import Reals.
+Require Import Psatz.
+Require Import QuantumLib.Complex.
+
+Local Open Scope nat_scope.
+
+(* This document contains the syntax of QBlue, which is an extension of Lambda/mu calculus 
+   with second quantization.
+   The inductive relation equiv defines the expression equivalence relations.
+ *)
+
+ 
