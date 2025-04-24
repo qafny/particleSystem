78da143dcdcff663e3aa80a67b955e9dc787645c
anff <anff@iastate.edu>
2025-04-23 23:52:25 -0500

    Update boson creator using index access

--- coq/QBlueSyntax.v
+++ coq/QBlueSyntax.v
@@ -62,26 +62,54 @@ Inductive paulimat: Type :=
 | paulix : C -> paulimat            (* X = c*[[0;1]; [1;0] ] *)
 | pauliy : C -> paulimat            (* Y = c*[[0;-i]; [i;0]] *)
 | pauliz : C -> paulimat           (* Z = c*[[1;0]; [0;-1]] *)
-| paulii : paulimat.
+| paulii : C -> paulimat.
+
 
 (* Hamiltonian type after transformation.
-Nested structure: (plus (tensor (app))) *)
+Nested structure: (plus (tensor (app )))) *)
 Definition lowprog := list (list (list paulimat)).
 
 (* ladder operator *)
-Definition ladder_anni (coef : R) : lowprog:=
+Definition ladder_anni (coef : R) : lowprog :=
 [[[paulix (Cmult C1 (RtoC ((1/2) * coef)))]]; [[pauliy ((Cmult (-Ci) (RtoC ((1/2) * coef))))%C]]].  
 
-Definition ladder_creator (coef : R) : lowprog:=
+Definition ladder_creator (coef : R) : lowprog :=
 [[[paulix (Cmult C1 (RtoC ((1/2) * coef)))]]; [[pauliy ((Cmult Ci (RtoC ((1/2) * coef))))%C]]].  
 
+(* projection operator |1><1| *)
+Definition projection_op (coef : R): lowprog :=
+[[[paulii (Cmult C1 (RtoC ((1/2) * coef)))]]; [pauliz (Cmult (-C1) (RtoC ((1/2) * coef)))]].
 
 (* Boson-qubit mapping *)
+(* 1st, id for sum; 2nd id for tensor *)
+Definition posi := nat * nat. 
+
+(* b_i^+ = SUM_{0 to Nb-1} sqrt(n+1) I_0 x ... x (+)_n x (-)_(n+1) ... x I_Nb *)
+Definition boson_creator (pos : posi) : lowprog :=
+  let (p1, p2) = pos in (* p1 is the id of the term in sum, p2 the one in tensor *)
+  if p1 =? p2 then ladder_anni (1)
+  else if p1 + 1 =? p2 then ladder_creator (sqrt (p2))
+  else [[[paulii]]]
+
+(* b_i = SUM_{1 to Nb} sqrt(n) I_0 x ... x (-)_(n-1) x (+)_(n) ... x I_Nb *)
+Definition boson_anni (pos : posi) : lowprog :=
+  let (p1, p2) = pos in (* p1 is the id of the term in sum, p2 the one in tensor *)
+  if p1 =? p2 then ladder_creator (1)
+  else if p1 + 1 =? p2 then ladder_anni (sqrt (p2))
+  else [[[paulii]]]
+
+(* n_i = SUM_{0 to Nb} n_i I_0 x ... |1><1|_ni ... x I_Nb *)
+Definition boson_n (pos : posi) : lowprog := 
+  let (p1, p2) = pos in (* p1 is the id of the term in sum, p2 the one in tensor *)
+  if p1 =? p2 then projection_op (p2)
+  else [[[paulii]]]
+
 (* I_i = I_0 x I_1 ... x I_Nb *)
+(*
 Fixpoint I (Nb: nat): lowprog :=
   match Nb with 
-  | 0 => [[[paulii]]]
-  | S n => [[[paulii]]] ++ (I n)
+  | 0 => [[[[paulii]]]]
+  | S n => [[[paulii]]]] :: (I n)
   end.
 
 (* b_i^+ = SUM_{0 to Nb-1} sqrt(n+1) I_0 x ... x (+)_n x (-)_(n+1) ... x I_Nb *)
@@ -117,5 +145,7 @@ Definition op_mult (a b : lowprog) : lowprog :=
 (* number operator: ni = b_i^+ b_i *)
 Definition boson_n (Nb : nat) : lowprog :=
   op_mult (boson_anni Nb) (boson_creator Nb).
+*)
 
 (* Jordan-Wigner transformation for fermions *)
+
