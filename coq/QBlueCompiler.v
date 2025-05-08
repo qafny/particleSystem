31cfe9407aec46fdd4c0502db7a576f378407439
anff <anff@iastate.edu>
2025-05-08 14:58:35 -0500

    Update Trotteriztion

--- coq/QBlueCompiler.v
+++ coq/QBlueCompiler.v
@@ -245,13 +245,13 @@ Definition snd_map (input : highprog) : lowprog :=
 
 (* The error bound for the Lie-Trotter *)
 (* Commutator: [A, B] = AB - BA *)
-Definition commutator (h1 h2 : lowprog) : lowprog :=
-  plus_plus_plus (plus_app_plus h1 h2) (mult_ampli_hplus (-C1)%C (plus_app_plus h2 h1)).
+Definition commutator (h1 h2 : lowprog_ten) : lowprog :=
+  [ten_app_ten h1 h2] ++ (mult_ampli_hplus (-C1)%C [ten_app_ten h2 h1]).
 
 (* Norm of H matrix *)
 Parameter norm : lowprog -> R.
 (* exp(-iHt) *)
-Parameter expH : R -> lowprog -> lowprog.
+Parameter expH : C -> lowprog -> lowprog.
 
 
 (* Drop first n elements of a list. *)
@@ -265,51 +265,47 @@ Fixpoint drop_nth {A : Set} (n : nat) (l : list A) : list A :=
 (* Eval compute in drop_nth 2 [10; 20; 30; 40]. 
 Returns [30; 40] *)
 
-(*
-(* sum_{γ2=γ1+1}^Γ [H_{γ2}, H_{γ1}] *)
-Definition comm_sums (gamma1 : nat) (hlist : list lowprog) : lowprog :=
-    let subl := drop_nth gamma1 hlist in
-    let h1 := hd [] subl in
-    let htail := tl subl in
-    let fix helper (ll : list lowprog) : lowprog :=
-        match ll with [] => []
-        | h2 :: rem => plus_plus_plus (commutator h2 h1) (helper rem)
-        end
-    in
-    helper htail.
 
+(* sum_{γ2=γ1+1}^Γ [H_{γ2}, H_{γ1}] *)
+Fixpoint comm_sums_helper (h2l : lowprog) (h1 : lowprog_ten) : lowprog :=
+  match h2l with 
+  | [] => []
+  | h2 :: rem => plus_plus_plus (commutator h2 h1) (comm_sums_helper rem h1) 
+  end.
+
+Definition comm_sums (gamma1 : nat) (hlist : lowprog) : lowprog :=
+  let subl := drop_nth gamma1 hlist in
+  match subl with 
+  | [] => []
+  | h :: rem => comm_sums_helper rem h
+  end.
 
 (* Outer sum: ∑_{γ1=1}^Γ || ∑_{γ2=γ1+1}^Γ [Hγ2, Hγ1] || *)
-Definition trotter_error_bound (t : R) (hlist : list lowprog) : R :=
-    let gamma := length hlist in
-    let fix helper (idx : nat) : R :=
-        (match idx with
-        | 0 => 0
-        | S k => let comm_sum := comm_sums idx hlist in
-            Rplus (norm comm_sum) (helper k)
-    end) in
-    (t*t/2) * helper gamma.
+Fixpoint trotter_error_bound_helper (idx : nat) (hlist : lowprog) : R :=
+  match idx with
+  | 0 => 0
+  | S k => let comm_sum := comm_sums idx hlist in
+      Rplus (norm comm_sum) (trotter_error_bound_helper k hlist)
+  end.
 
+Definition trotter_error_bound (t : R) (hlist : lowprog) : R :=
+  let gamma := length hlist in
+  (t*t/2) * (trotter_error_bound_helper gamma hlist). 
 
-Fixpoint e_split (t : R) (hlist : list lowprog) : lowprog :=
+(* Approx = exp(-itH_1)exp(-itH_2) ... *)
+Fixpoint e_split (t : R) (hlist : lowprog) : lowprog :=
     match hlist with [] => []
-    | h :: ht => plus_app_plus (expH (-t) h) (e_split t ht)
-    end.
-
-Fixpoint sum_lowprog (l : list lowprog) : lowprog :=
-    match l with
-    | [] => []
-    | x :: xs => plus_plus_plus x (sum_lowprog xs)
+    | h :: ht => plus_app_plus (expH (Cmult (-Ci) t) [h]) (e_split t ht)
     end.
 
 (* theorem: Tight error bound for the  rst-order Lie-Trotter formula *)
 Theorem lie_trotter_error_bound :
-  forall (t : R) (hlist : list lowprog),
+  forall (t : R) (hlist : lowprog),
   let approx := e_split t hlist in
-  let gold := expH (-t) (sum_lowprog hlist) in
-  norm (plus_plus_plus approx (mult_ampli_hplus (-C1) gold)) <=
-    trotter_error_bound t hlist.
+  let gold := expH (Cmult (-Ci) t) hlist in
+  Rle (norm (plus_plus_plus approx (mult_ampli_hplus (-C1) gold)))
+    (trotter_error_bound t hlist).
 
 Proof. Admitted.
 
-*)
+
