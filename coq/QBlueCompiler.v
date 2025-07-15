1c176243352014151d70bb3d97ee547c7dad4656
anff <anff@iastate.edu>
2025-07-15 16:07:49 -0500

    Add sythesization def for IBM and Indiana

--- coq/QBlueCompiler.v
+++ coq/QBlueCompiler.v
@@ -33,6 +33,14 @@ Definition paulimat_eqb (a b : paulimat) : bool :=
 Definition lowprog_ten := (C * nat * (nat -> paulimat)) %type.
 Definition lowprog := list lowprog_ten.
 
+(* This is for trotterization. Maybe need to modify later *)
+(* exp(-i t H) *)
+Parameter expH : R -> lowprog -> lowprog.
+
+(* This is the input for sythesization *)
+Definition ugate := (R * lowprog) %type.
+Parameter exp_ugate : R -> lowprog -> ugate. (* exp(-i r H) *)
+
 Definition app_pauli (s1 s2 : paulimat) : (C * paulimat) :=
   match s1, s2 with
   | paulii, x => (C1, x)
@@ -426,9 +434,6 @@ Definition commutator_ts (h1 : lowprog_ten) (h2 : lowprog) : lowprog :=
 
 (* Norm of H matrix *)
 Parameter norm : lowprog -> R.
-(* exp(-iHt) *)
-Parameter expH : C -> lowprog -> lowprog.
-
 
 (* Drop first n elements of a list. *)
 Fixpoint drop_nth {A : Set} (n : nat) (l : list A) : list A :=
@@ -463,7 +468,7 @@ Definition trotter_error_bound (t : R) (hlist : lowprog) : R :=
 (* Approx = exp(-itH_1)exp(-itH_2) ... *)
 Fixpoint e_split (t : R) (hlist : lowprog) : lowprog :=
     match hlist with [] => []
-    | h :: ht => plus_app_plus (expH (Cmult (-Ci) t) [h]) (e_split t ht)
+    | h :: ht => plus_app_plus (expH t [h]) (e_split t ht)
     end.
 
 (* theorem: Tight error bound for the first-order Lie-Trotter formula *)
@@ -471,7 +476,7 @@ Fixpoint e_split (t : R) (hlist : lowprog) : lowprog :=
 Theorem lie_trotter_error_bound :
   forall (t : R) (hlist : lowprog),
   let approx := e_split t hlist in
-  let gold := expH (Cmult (-Ci) t) hlist in
+  let gold := expH t hlist in
   Rle (norm (plus_plus_plus approx (mult_ampli_hplus (-C1) gold)))
     (trotter_error_bound t hlist).
 
@@ -509,7 +514,7 @@ Definition e_split_suzuki (t : R) (hlist : lowprog) : lowprog :=
 Theorem suzuki_second_order_error_bound :
   forall (t : R) (hlist : lowprog),
   let approx := e_split_suzuki t hlist in
-  let gold := expH (Cmult (-Ci) t) hlist in
+  let gold := expH t hlist in
   Rle (norm (plus_plus_plus approx (mult_ampli_hplus (-C1) gold)))
       (suzuki_error_bound t hlist).
 
@@ -777,10 +782,8 @@ Definition Hgad (k : nat) (lambda : R) (hori : lowprog) : lowprog :=
     
   
 
-
 (* synthesization *)
-Local Open Scope ucom_scope.
-(*1. to basic gates of CNOT, H, Rz, Collect parity on the last qubit *)
+(* 1. to basic gates of CNOT, H, Rz, Collect parity on the last qubit *)
 (* Map X and Y to Z, X = HZH, Y=Rz(pi/2) HZH Rz(-pi/2) *)
 (* nbit: # of bits in circuit; curbit: id of the pauli in the string; s: the current pauli to convert 
 return: the converting matrix: for Y, return Rz; H and H; Rz) *)
@@ -823,7 +826,91 @@ Definition synth_digital (nqubit : nat) (pauli_str : nat -> paulimat) : base_uco
   useq left (useq mid right).
 
 
+(* 2. to gates of IBM analog, X, Z, ZZ gates *)
+(* fill_pl 3 2 X => I tensor I tensor X *)
+Fixpoint inb {A : Type} (eqb : A -> A -> bool) (a : A) (l : list A) : bool :=
+  match l with
+  | [] => false
+  | x :: xs => if eqb x a then true else inb eqb a xs
+  end.
 
-Local Close Scope ucom_scope.
+Definition fill_pl (nbit : nat) (reg : list nat) (p : paulimat) : lowprog_ten :=
+  let f := (fun id => if (inb Nat.eqb id reg) then p else paulii) in (C1, nbit, f).
+
 Local Close Scope nat_scope.
+
+(* H = exp(-i pi/4 X) exp(-i pi/4 Z) exp(-i pi/4 X). For IBM *)
+(* H unitary gate on X and Z basis.
+   nbit: # of bits in circuit; qid: id of the current bit *)
+Definition H_u (nbit qid : nat) : list ugate := 
+  let xu := exp_ugate (PI/4) [fill_pl nbit [qid] paulix] in
+  let zu := exp_ugate (PI/4) [fill_pl nbit [qid] pauliz] in
+  xu :: zu :: xu :: nil. 
+
+(* S = exp(-i pi/4 Z) *)
+Definition S_u (nbit qid : nat) : list ugate := 
+  let zu := exp_ugate (PI/4) [fill_pl nbit [qid] pauliz] in zu :: nil.
+
+(* S^+ = exp(-i 7 pi/4 Z) *)
+Definition SDag_u (nbit qid : nat) : list ugate := 
+  let zu := exp_ugate (7 * PI/4) [fill_pl nbit [qid] pauliz] in zu :: nil. 
+
+(* Find the nonI in pauli_str. qid starts from the length of pauli_str. *)
+Fixpoint find_nonI (qid : nat) (pauli_str : nat -> paulimat) : list nat :=
+  match qid with 
+  | 0 %nat => []
+  | S n => let aux := find_nonI n pauli_str in
+    (if (paulimat_eqb (pauli_str n) paulii) then aux else n :: aux)
+  end.  
+
+(* Input is 2-local. Convert to X, Z, ZZ basis *)
+Fixpoint synth_analog_ibm_helper (ml : list nat) (nbit : nat) (pauli_str : nat -> paulimat) : 
+  (list ugate) * (list ugate) :=
+  match ml with 
+  | [] => ([], [])
+  | m :: ax => let (left, right) := synth_analog_ibm_helper ax nbit pauli_str in
+    let hu := H_u nbit m in
+    let su := S_u nbit m in 
+    let sdagu := SDag_u nbit m in
+    let (app1, app2) := 
+      match (pauli_str m) with 
+      | paulix => (hu, hu)
+      | pauliy => (su ++ hu, hu ++ sdagu)
+      | _ => ([], []) 
+      end in
+    (left ++ app1, app2 ++ right) end.
+
+Definition synth_analog_ibm (t : R) (nbit : nat) (pauli_str : nat -> paulimat) 
+  : list ugate :=
+  let ml := find_nonI nbit pauli_str in 
+  if Nat.leb (length ml) 2 then
+    let mid := exp_ugate t [fill_pl nbit ml pauliz] in
+    let (left, right) := synth_analog_ibm_helper ml nbit pauli_str in
+    left ++ [mid] ++ right
+  else [].
+
+
+(* 3. Input is any-local. Convert to Z, X...X basis *)
+Fixpoint synth_analog_indiana_helper (ml : list nat) (nbit : nat) (pauli_str : nat -> paulimat) : 
+  (list ugate) * (list ugate) :=
+  match ml with 
+  | [] => ([], [])
+  | m :: ax => let (left, right) := synth_analog_indiana_helper ax nbit pauli_str in
+    let hu := H_u nbit m in
+    let su := S_u nbit m in 
+    let sdagu := SDag_u nbit m in
+    let (app1, app2) := 
+      match (pauli_str m) with 
+      | pauliy => (su, sdagu)
+      | pauliz => (hu, hu)
+      | _ => ([], []) 
+      end in
+    (left ++ app1, app2 ++ right) end.
+
+Definition synth_analog_indiana (t : R) (nbit : nat) (pauli_str : nat -> paulimat) 
+  : list ugate :=
+  let ml := find_nonI nbit pauli_str in 
+  let mid := exp_ugate t [fill_pl nbit ml pauliz] in
+  let (left, right) := synth_analog_indiana_helper ml nbit pauli_str in
+  left ++ [mid] ++ right.
 
