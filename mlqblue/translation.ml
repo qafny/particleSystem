827322f16c2eaa0e22bcc9e55d3d7ad6411b8d3a
anff <anff@iastate.edu>
2026-02-05 09:39:09 -0600

    Add protection about translation in ocaml

--- mlqblue/translation.ml
+++ mlqblue/translation.ml
@@ -13,10 +13,16 @@ open Util
 let trotterStd_IBMDigital ?(verbose=false) (err : float) (t : float) (lp : lowprog) (nq : int) =
   if verbose then dbg "---- Trotterization (1st-order) -> IBMDigital circuits: ----";
   let r = trotter_step err t lp in
-  let npau = r * (Stdlib.List.length lp) in
-  if verbose then dbg "Dealing with %d pauli strings" npau;
-  translate_lowp2circ_std err t lp nq 
+  let nterm = Stdlib.List.length lp in
+  let npau = r * nterm in
 
+  (* rfactor must be very close to 1 to make sure error <= expected error  *)
+  let rfactor = exp( (float_of_int nterm) *. t /. (float_of_int r)) in
+  if verbose then dbg "Dealing with %d pauli strings; relaxation factor: %f." npau rfactor;
+  try
+    translate_lowp2circ_std err t lp nq 
+  with exn -> dbg "trotterStd_IBMDigital raise EXN: %s" (Printexc.to_string exn);
+    raise exn
 
 let trotterQDrift_IBMDigital ?(verbose=false) (err : float) (t : float) (lp : lowprog) (nq : int) =
   (* Set seed to reproduce results *)
@@ -25,8 +31,14 @@ let trotterQDrift_IBMDigital ?(verbose=false) (err : float) (t : float) (lp : lo
   if verbose then dbg "---- Trotterization (QDrift) -> IBMDigital circuits: ----";
   let npau = qdrift_step err t lp in
   let lambda = sum_w lp (Stdlib.List.length lp) in
-  if verbose then dbg "Dealing with %d pauli strings; lambda = %f" npau lambda;
-  translate_lowp2circ_qdrift err t lp nq 
+
+  (* rfactor must be very close to 1 to make sure error <= expected error  *)
+  let rfactor = exp(2.0 *. lambda *. t /. (float_of_int npau)) in
+  if verbose then dbg "Dealing with %d pauli strings; lambda = %f; relaxation factor: %f." npau lambda rfactor;
+  try
+    translate_lowp2circ_qdrift err t lp nq 
+  with exn -> dbg "trotterQDrift_IBMDigital raise EXN: %s" (Printexc.to_string exn);
+    raise exn
 
 
 
