e6b0fc2a3393ef21d41cab75afdcea753197ad0c
anff <anff@iastate.edu>
2026-03-11 23:37:36 -0500

    Change compilation time limit to be 10 minutes

--- mlqblue/analyze_utility.ml
+++ mlqblue/analyze_utility.ml
@@ -13,7 +13,7 @@ open Voqc.Qasm
 open Translation
 open Qblue_util
 
-let translation_timeout_seconds = 3600
+let translation_timeout_seconds = 600
 exception Compile_timeout of int
 
 let rec count_U1_ocaml (c : coq_U ucom) : int =
