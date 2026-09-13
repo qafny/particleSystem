75df0dae4e76e8db780c0c5feba8227ad9e17efc
liyili2 <liyili2@illinois.edu>
2026-09-12 21:04:26 -0500

    update.

--- coq/QBlueSynthProof.v
+++ coq/QBlueSynthProof.v
@@ -350,112 +350,73 @@ Proof.
 Qed.
 
 
-
-
-(* U† A U *)
-Definition conj {n} (U A : Matrix n n) : Matrix n n :=
-  Mmult (U†) (Mmult A U).
-
-
-Fixpoint prod {n : nat} (us : list (Matrix n n)) : Matrix n n :=
-  match us with
-  | [] => I n
-  | u :: tl => (prod tl) × u   (* gives U_n ... U_1 if list is [U1;...;Un] *)
+Fixpoint add_h (n:nat) (f: nat -> paulimat) : Square (2^n) :=
+  match n with
+   | 0 => I 1
+   | S m => if is_i (f m) then add_h m f ⊗ I 2 else add_h m f ⊗ hadamard
   end.
 
-(*
-Lemma big_conj_goal: forall {n:nat} (us : list (Matrix n n)) (P : Matrix n n),
-  (prod us)† × P × (prod us) = P'. *)
+Definition long_x_inter (n:nat) (f: nat -> paulimat) t :=
+  add_h n f × (fst (long_z n f t)) × add_h n f.
 
+Fixpoint add_front_in (n:nat) (f: nat -> paulimat) : Square (2^n) :=
+  match n with
+   | 0 => I 1
+   | S m => match (f m) with
+              | pauliy => add_front_in m f ⊗ phase_shift (PI / (IZR 2))
+              | pauliz => add_front_in m f ⊗
+                      (phase_shift (PI / (IZR 2)) × x_rotation (PI / (IZR 2)) × (phase_shift (PI / (IZR 2))))
+              | _ => add_front_in m f ⊗ I 2
+            end
+  end.
 
-Lemma conj_mul {n} (A B P : Matrix n n) :
-  ((A × B)†) × P × (A × B)
-  = (B†) × ((A†) × P × A) × B.
-Proof.
-  (* typical steps: expand dagger of product, reassociate *)
-  rewrite Mmult_adjoint.         (* (A×B)† = B† × A† *)
-  repeat rewrite Mmult_assoc.  (* reassociate × *)
-  reflexivity.
-Qed.
+Definition inter_in_synth (n:nat) (f: nat -> paulimat) t :=
+  (adjoint (add_front_in n f)) × add_h n f × long_x_inter n f t × add_h n f × (add_front_in n f).
 
-
-Lemma conj_prod {n} (us : list (Matrix n n)) (P : Matrix n n) :
-  (prod us)† × P × (prod us)
-  = fold_left (fun acc u => conj u acc) us P.
+Lemma synth_in_machine_same: forall n amp f,
+                    inter_in_synth n f amp = (fst (long_z n f amp)).
 Proof.
 Admitted.
-(*
-  induction us as [|u tl IH].
-  - simpl. (* prod [] = I; fold_left ... [] P = P *)
-    (* rewrite with dagger_I, I_mult, mult_I, etc. *)
-    (* goal becomes P = P *)
-    now simp_matrix.  (* or your library’s simp tactic *)
-  - simpl.
-    (* prod (u::tl) = prod tl × u *)
-    (* Use conj_mul with A := prod tl, B := u *)
-    rewrite conj_mul.
-    (* reduce using IH *)
-    rewrite IH.
-    (* fold_left unfolding *)
-    simpl.
-    (* then reassociate as needed *)
-    repeat rewrite Mmult_assoc.
-    reflexivity.
-Qed. *)
 
-Definition conj_by_list (us : list U) (P : Pauli) : Pauli :=
-  fold_left (fun acc u => conj u acc) us P.
 
-(* Theorem conj_prod_computes (us : list U) (P : Pauli) :
-  (prod us)† × P × (prod us) = conj_by_list us P.
+
+Definition conj_paulis (a b:paulimat) :=
+  match (a,b) with (paulix,paulix) => (C1,paulii)
+                 | (pauliy,pauliy) => (C1,paulii)
+                 | (pauliz,pauliz) => (C1,paulii)
+                 | (paulii,paulii) => (C1,paulii)
+                 | (paulix,pauliy) => (Ci,pauliz)
+                 | (paulix,pauliz) => (-Ci,pauliy)
+                 | (paulix,paulii) => (C1,paulix)
+                 | (pauliy,paulix) => (-Ci,pauliz)
+                 | (pauliy,pauliz) => (Ci,paulix)
+                 | (pauliy,paulii) => (C1,pauliy)
+                 | (pauliz,paulix) => (Ci,pauliy)
+                 | (pauliz,pauliy) => (-Ci,paulix)
+                 | (pauliz,paulii) => (C1,pauliz)
+                 | (paulii,paulix) => (C1,paulix)
+                 | (paulii,pauliy) => (C1,pauliy)
+                 | (paulii,pauliz) => (C1,pauliz)
+  end.
+
+Definition inter_pauli (a:paulimat) :=
+  match a with paulii => I 2
+             | paulix => σx
+             | pauliy => σy
+             | pauliz => σz
+  end.
+
+Lemma conj_correct : forall a b , inter_pauli a × inter_pauli b 
+           = (fst (conj_paulis a b)) .* inter_pauli (snd (conj_paulis a b)).
 Proof.
-  rewrite conj_prod.
-  (* goal: fold_left (fun acc u => conj u acc) us P = conj_by_list us P *)
-  reflexivity.
-Qed. *)
-
-
-Lemma conj_prod_computes :
-  forall us P,
-    denote_Pauli (conj_by_list us P)
-    = (denote_U (prod us))† × denote_Pauli P × denote_U (prod us).
-Proof.
-  induction us as [|u us IH]; intros P.
-  - (* us = [] *)
-    cbv [conj_by_list prod]; cbn.
-    (* goal becomes denote_Pauli P = I† × denote_Pauli P × I *)
-    (* finish with matrix identity lemmas *)
-    simp_matrix.  (* or: rewrite Mmult_1_l, Mmult_1_r, dagger_I, etc. *)
-  - (* us = u :: us *)
-    cbv [conj_by_list]; cbn.
-    (* fold_left over (u :: us) reduces to fold_left over us with initial conj u P *)
-    (* LHS: denote_Pauli (conj_by_list us (conj u P)) *)
-    rewrite IH.
-    (* Now use conj_sound to replace denote_Pauli (conj u P) with U† P U *)
-    rewrite conj_sound.
-    (* Now rearrange products to match (prod (u::us))† P (prod (u::us)) *)
-    (* You also need a lemma relating denote_U (prod (u::us)) to denote_U u × denote_U (prod us) *)
-    (* and dagger of a product: (AB)† = B† A† *)
-    simp_matrix.  (* or explicit associativity rewrites *)
-Qed.
-
-
-
-Lemma conj_H_X : conj H X = Z.  (* i.e., H† X H = Z; since H†=H *)
-Lemma conj_H_Z : conj H Z = X.
-Lemma conj_H_Y : conj H Y = - Y.
-
-Lemma conj_S_X : conj S X = Y.      (* depending on your convention; may be -Y *)
-Lemma conj_S_Y : conj S Y = - X.
-Lemma conj_S_Z : conj S Z = Z.
-
-
-Goal (prod us)† × P × (prod us) = P'.
-Proof.
-  rewrite conj_prod.
-  (* now goal is fold_left (fun acc u => conj u acc) us P = P' *)
-  (* if us is concrete, simp + rewrite using the conj_* lemmas *)
-  repeat (cbn; try rewrite conj_H_X; try rewrite conj_H_Z; try rewrite conj_S_X; ...).
-  reflexivity.
+  intros. unfold conj_paulis,inter_pauli in *.
+  destruct a;simpl in *. destruct b;simpl in *.
+  1-4:solve_matrix_fast.
+  destruct b;simpl in *.
+  1-4:solve_matrix_fast.
+  destruct b;simpl in *.
+  1-4:solve_matrix_fast.
+  destruct b;simpl in *.
+  1-4:solve_matrix_fast.
 Qed.
 
